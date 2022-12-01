0e3199a33c52500ad139809aa0e87eb12908f53e
Alejandro Zapata <alzapata317@gmail.com>
2022-11-30 20:42:49 -0500

    Added links to work profiles and changed to overall color to dark removed the about me page and planning a small introduction on the first page

---
+++ js/index.js
@@ -0,0 +1,28 @@
+const hiddenElements = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+hiddenElements.forEach((el) => observer.observe(el));
+
+const hiddenElements2 = document.querySelectorAll('.hidden2');
+
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show2');
+        } else {
+            entry.target.classList.remove('show2');
+        }
+    });
+});
+hiddenElements2.forEach((el) => observer2.observe(el));
